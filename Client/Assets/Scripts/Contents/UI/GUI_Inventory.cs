b6ae8181ff75de26927ab878e4471d918fd1ed68
dochi486 <93mskim@gmail.com>
2024-01-03 21:50:44 +0900

    아이템 탭 토글 적용, 아이템 버튼 클릭효과 적용

--- Client/Assets/Scripts/Contents/UI/GUI_Inventory.cs
+++ Client/Assets/Scripts/Contents/UI/GUI_Inventory.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using UnityEngine;
 using UnityEngine.UI;
+using UniRx;
 
 class GUI_Inventory : GUIBase
 {
 
     [SerializeField] private LoopVerticalScrollRect loopRect;
     [SerializeField] private UI_Item baseItem;
+    [SerializeField] private Toggle weaponToggle;
+    [SerializeField] private Toggle shieldToggle;
+    [SerializeField] private Toggle shoesToggle;
+    [SerializeField] private Toggle ringsToggle;
 
     public class OpenParam : IGUIOpenParam
     {
@@ -18,6 +23,7 @@ class GUI_Inventory : GUIBase
     private void Awake()
     {
         m_back_button.onClick.AddListener(OnBackButtonClick);
+        BindUI();
     }
 
     override public void Open(IGUIOpenParam in_param)
@@ -40,6 +46,29 @@ class GUI_Inventory : GUIBase
 
         loopRect.UpdatePollTotalCount(64);
         loopRect.RefreshScrollRect();
+    }
+
+    void BindUI()
+    {
+        weaponToggle.OnValueChangedAsObservable().Subscribe(isOn =>
+        {
+            Debug.Log(isOn ? "무기" : "무기꺼짐");
+        });
+
+        shieldToggle.OnValueChangedAsObservable().Subscribe(isOn =>
+        {
+            Debug.Log(isOn ? "방패" : "방패꺼짐");
+        });
+
+        shoesToggle.OnValueChangedAsObservable().Subscribe(isOn =>
+        {
+            Debug.Log(isOn ? "신발" : "신발꺼짐");
+        });
+
+        ringsToggle.OnValueChangedAsObservable().Subscribe(isOn =>
+        {
+            Debug.Log(isOn ? "반지" : "반지꺼짐");
+        });
     }
 
     //TODO: 스크롤 컨텐츠 오브젝트 풀링.. 
