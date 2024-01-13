3916d1f687fad24bb88e63cf405759f18ea5b3d5
baehun92 <baehun92@naver.com>
2024-01-13 23:23:46 +0900

    빌드시 DataTable 이 포함되도록 작업. ㄴ Datatable 을 Resource/DataTables 에 복사. ㄴ 유니티에서 파일을 읽을 수 있도록 확장자 변경.

--- Client/Assets/Editor/BuildScript.cs
+++ Client/Assets/Editor/BuildScript.cs
@@ -3,32 +3,25 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEditor.Build.Reporting;
+using System.IO;
 
+// 젠킨스에서 사용되는 빌드 스크립트입니다.
 public class BuildScript
 {
     [MenuItem("Build/Build Android")]
     public static void AndroidBuild()
     {
-        BuildPlayerOptions options = new BuildPlayerOptions();
-        // 씬 추가
-        List<string> scenes = new List<string>();
-        foreach (var scene in EditorBuildSettings.scenes)
-        {
-            if (!scene.enabled) continue;
-            scenes.Add(scene.path);
-        }
-        options.scenes = scenes.ToArray();
-        Debug.Log("Included scene : " + scenes);
+        Debug.Log("Android Build Start");
 
-        // 타겟 경로(빌드 결과물이 여기 생성됨)
-        options.locationPathName = "Build/ProjectD.apk";
-        
-        // 설정이 필요없을수 있다. 테스트 필요.
-        // 빌드 타겟
-        options.target = BuildTarget.Android;
+        // 데이터 폴더를 복사합니다.
+        CopyDataTableAndChnageExtension();
+
+        // 빌드 옵션 설정.
+        // 디렉토리 및 프로젝트 이름을 외부에서 가져오도록 변경하는것을 고려해야됨.
+        var build_option = MakeBuildOption(BuildTarget.Android, "Build", "ProjectD.apk");
 
         // 빌드
-        BuildReport report = BuildPipeline.BuildPlayer(options);
+        BuildReport report = BuildPipeline.BuildPlayer(build_option);
         BuildSummary summary = report.summary;
 
         if (summary.result == BuildResult.Succeeded)
@@ -44,26 +37,16 @@ public class BuildScript
     [MenuItem("Build/Build Aos")]
     public static void iOSBuild()
     {
-        BuildPlayerOptions options = new BuildPlayerOptions();
-        // 씬 추가
-        List<string> scenes = new List<string>();
-        foreach (var scene in EditorBuildSettings.scenes)
-        {
-            if (!scene.enabled) continue;
-            scenes.Add(scene.path);
-        }
-        options.scenes = scenes.ToArray();
-        Debug.Log("Included scene : " + scenes);
+        // 데이터 폴더를 복사합니다.
+        CopyDataTableAndChnageExtension();
 
-        // 타겟 경로(빌드 결과물이 여기 생성됨)
-        options.locationPathName = "Build/ProjectD.ios";
 
-        // 설정이 필요없을수 있다. 테스트 필요.
-        // 빌드 타겟
-        options.target = BuildTarget.iOS;
-        
+        // 빌드 옵션 설정.
+        // 디렉토리 및 프로젝트 이름을 외부에서 가져오도록 변경하는것을 고려해야됨.
+        var build_option = MakeBuildOption(BuildTarget.iOS, "Build", "ProjectD.ios");
+
         // 빌드
-        BuildReport report = BuildPipeline.BuildPlayer(options);
+        BuildReport report = BuildPipeline.BuildPlayer(build_option);
         BuildSummary summary = report.summary;
 
         if (summary.result == BuildResult.Succeeded)
@@ -76,8 +59,125 @@ public class BuildScript
         }
     }
 
-    [MenuItem("Build/Build Test")]
-    public static void Test()
+    // 빌드 옵션을 설정한다.
+    //  1. 씬 추가.
+    //  2. 빌드 타겟 설정.
+    //  3. 빌드 결과물 경로 및 이름 설정.
+    private static BuildPlayerOptions MakeBuildOption(BuildTarget in_build_target, string in_output_path, string in_file_name)
     {
+        BuildPlayerOptions options = new BuildPlayerOptions();
+        // 씬 추가
+        List<string> scenes = new List<string>();
+        foreach (var scene in EditorBuildSettings.scenes)
+        {
+            if (!scene.enabled) continue;
+            scenes.Add(scene.path);
+        }
+        options.scenes = scenes.ToArray();
+        Debug.Log("MakeBuildOption - Included scene : " + scenes);
+
+
+        // 빌드 타겟
+        options.target = in_build_target;
+        Debug.Log("MakeBuildOption - target : " + in_build_target);
+
+        // 타겟 경로(빌드 결과물이 여기 생성됨)
+        var location_path = Path.Combine(in_output_path, in_file_name);
+        options.locationPathName = location_path;
+        Debug.Log("MakeBuildOption - output info : " + location_path);
+
+        return options;
+    }
+
+    // 빌드 파일에 DataTable 을 포함시키기위하여 추가된 함수.
+    // 동작
+    //  1. Resource/DataTables 경로에 엑셀을 복사합니다.
+    //     ㄴ 만약 Resource/DataTables 폴더가 이미 존재하는 경우 삭제 후 복사를 진행합니다.
+    //  2. 엑셀은 Unity 에서 인식을 하지 못하기 대문에 .text 파일로 변경합니다.
+    private static void CopyDataTableAndChnageExtension()
+    {
+        Debug.Log("Copy DataTables start");
+
+        string copy_path = Path.Combine("Assets", "Resources", "DataTables");
+        string source_path = Path.Combine("..", "DataTable");
+        string target_extension = ".xlsx";
+        string chagen_extension = ".text";
+
+        // 이전에 복사된 데이터 디렉토리 삭제.
+        // 데이터가 꼬이는 문제를 방지하기 위함.
+        if (Directory.Exists(copy_path))
+        {
+            Directory.Delete(copy_path, true /* recursive */);
+            Debug.Log("Delet old DataTables directory : " + copy_path);
+        }
+
+        CopyDirectory(source_path, copy_path);
+
+        Debug.Log("Copy DataTables finish");
+
+
+
+        Debug.Log("Change extension start");
+
+        // 확장자 변경.
+        ChangeFileExtensions(copy_path, target_extension, chagen_extension);
+
+        Debug.Log("Change extension finish");
+    }
+
+
+    public static void CopyDirectory(string in_source_path, string in_target_path)
+    {
+        // 대상 디렉토리가 존재하지 않으면 생성합니다.
+        Directory.CreateDirectory(in_target_path);
+
+        // 현재 경로의 파일과 폴더 목록을 가져옵니다.
+        string[] entries = Directory.GetFileSystemEntries(in_source_path);
+
+        foreach (string entry in entries)
+        {
+            string targetEntryPath = Path.Combine(in_target_path, Path.GetFileName(entry));
+
+            if (Directory.Exists(entry))
+            {
+                // 폴더인 경우 재귀적으로 복사합니다.
+                CopyDirectory(entry, targetEntryPath);
+            }
+            else
+            {
+                // 파일인 경우 복사합니다.
+                File.Copy(entry, targetEntryPath, true); // true는 기존 파일을 덮어쓰도록 합니다.
+            }
+        }
+    }
+
+    public static void ChangeFileExtensions(string in_source_path, string in_target_extension, string in_change_extension)
+    {
+        // 현재 경로의 파일과 폴더 목록을 가져옵니다.
+        string[] entries = Directory.GetFileSystemEntries(in_source_path);
+
+        foreach (string entry in entries)
+        {
+            string targetEntryPath = Path.Combine(in_source_path, Path.GetFileName(entry));
+
+            if (Directory.Exists(entry))
+            {
+                // 폴더인 경우 재귀적으로 경로를 변경해준다.
+                ChangeFileExtensions(targetEntryPath, in_target_extension, in_change_extension);
+            }
+            else
+            {
+                // 파일인 경우 target 확장자인지 확인.
+                string extension = Path.GetExtension(entry);
+                if (extension.Equals(in_target_extension))
+                {
+                    // 확장자가 변경된 경로 생성.
+                    string changed_path = Path.ChangeExtension(entry, in_change_extension);
+
+                    // 파일 변경.
+                    File.Move(entry, changed_path);
+                }
+            }
+        }
     }
 }
