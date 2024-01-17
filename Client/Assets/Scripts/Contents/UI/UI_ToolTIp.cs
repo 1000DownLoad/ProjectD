fe8fca9dd82973f6f79583d6199bbcdeb5e6b797
Myongsu Kim <93mskim@gmail.com>
2024-01-17 23:31:31 +0900

    ✨툴팁 프리팹, 스크립트 추가

---
+++ Client/Assets/Scripts/Contents/UI/UI_ToolTIp.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UI_ToolTIp : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI itemDescription;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void SetTooltipText()
+    {
+
+    }
+}
