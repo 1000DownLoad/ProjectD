67bc7572aaaf271e4bcab972b028415cae6986b4
baehun92 <baehun92@naver.com>
2024-01-19 01:03:51 +0900

    DataTable 에 CONST 기능 추가. ㄴ커밋 누락된 WorkBook 적용. ㄴ itemDataTable 에 Const 로드 기능 추가.

--- Client/Assets/Scripts/Contents/DataTable/DataTable-Item.cs
+++ Client/Assets/Scripts/Contents/DataTable/DataTable-Item.cs
@@ -17,6 +17,13 @@ namespace DataTable
     // 타입과 엑셀 테이블 명칭을 맞춰주세요.
     public class ItemDataTable : DataTableBase<ItemDataTable>, IDataTable
     {
+        #region CONST
+
+         public int CONST_FATIGUE_RECOVER_TIME { get; private set; }
+
+        #endregion
+
+
         // 한번 데이터가 쓰이면 변경할수 없다.
         // Clear 금지.
 
@@ -31,12 +38,22 @@ namespace DataTable
         public void LoadCommonDataTable()
         {
             WorkBook book = GetCommonRowData();
+
+            // CONST 를 가장먼저 세팅합니다.
+            if (book.Contains("CONST") == false)
+            {
+                Debug.LogError("ItemDataTable - CONST sheet not found");
+                Application.Quit();
+            }
+
+            book.Foreach("CONST", PraseConstRowData);
+
+
             if (book.Contains("ITEM") == false)
             {
                 Debug.LogError("ItemDataTable - ITEM sheet not found");
                 Application.Quit();
-            }
-               
+            }  
 
             // Capacity 를 지정하여 딕셔너리 생성.
             m_common_item_data = new Dictionary<ItemType, ItemData>(book.GetRowCount("ITEM"));
@@ -52,6 +69,24 @@ namespace DataTable
             m_common_item_data.TryGetValue(in_item_type, out var out_data);
 
             return out_data;
+        }
+
+        public void PraseConstRowData(Row in_row) 
+        {
+            // 여기서 에러가 발생한다면 엑셀 쓰레기값을 확인해보자.
+            var name = in_row[0].String;
+            switch (name)
+            {
+                case "CONST_FATIGUE_RECOVER_TIME":
+                {
+                    CONST_FATIGUE_RECOVER_TIME = in_row[1].Integer;
+                    break;
+                }
+                default:
+                    // 일치하는게 없다면 데이터 오류!
+                    Application.Quit();
+                    break;
+            }
         }
 
         public void PraseCommonItemRowData(Row in_row) 
